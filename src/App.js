46f2723d49476d28ab97606cb1c37a3b50ea5ee5
FacundoNunez1 <fakundo1213@gmail.com>
2023-10-03 18:58:37 -0300

    creacion de datos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import "./App.css"
+import Dato from "./Database/Data.js"
+import { useEffect, useState } from "react";
 function App() {
+  const [casa, setCasa] = useState("");
+  useEffect(() => {
+    setCasa(Dato[0])
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          branch dev<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main">
+        <p>Estás en: InfoCasas</p>
+        <div className="DetallePropiedadImagen">
+          {Array.isArray(casa.imgsrc) ? (
+            <div>
+              {casa.imgsrc.map((item, index) => (
+                <img
+                  key={index}
+                  src={item}
+                  alt={`Imagen ${index}`}
+                  className="image-size"
+                />
+              ))}
+            </div>
+          ) : (
+            <p>No hay imágenes disponibles</p>
+          )}
+        </div>
+        <div className="carta">
+          <h3>{casa.title}</h3>
+          <p>Precio de {casa.tipoVenta} :{casa.tipoMoneda} {casa.precio}</p>
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
+export default App
