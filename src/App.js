745a83fece2971ae01d785d7800be409ab23a552
blinksore <velah027@gmail.com>
2023-10-06 20:20:25 -0300

    Acoplar dev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import TextField from "@mui/material/TextField";
 import "./App.css";
 
 function App() {
-  const [seleccion, setSeleccion] = React.useState('venta');
+  const [seleccion, setSeleccion] = React.useState("venta");
 
   const handleChange = (event, newSeleccion) => {
     setSeleccion(newSeleccion);
@@ -14,10 +14,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className='Navbar'>
+      <div className="Navbar">
         <h1>Navbar</h1>
       </div>
-      <div className='SearchBackground'>
+      <div className="SearchBackground">
         <div className="Search">
           <Box>
             <ToggleButtonGroup
@@ -26,24 +26,24 @@ function App() {
               exclusive
               onChange={handleChange}
               aria-label="Platform"
-              id='opciones'
+              id="opciones"
             >
               <ToggleButton value="venta">Venta</ToggleButton>
               <ToggleButton value="alquiler">Alquiler</ToggleButton>
-              <ToggleButton value="alquilertemporal">Alquiler Temporal</ToggleButton>
+              <ToggleButton value="alquilertemporal">
+                Alquiler Temporal
+              </ToggleButton>
             </ToggleButtonGroup>
             <TextField id="search" label="Localidad" variant="outlined" />
           </Box>
         </div>
       </div>
 
-      <div className='Carousel'>
-        <h1>
-          Carousel
-        </h1>
+      <div className="Carousel">
+        <h1>Carousel aqui</h1>
       </div>
-      <div className='Otros'>
-        <h1>Otros</h1>
+      <div className="Otros">
+        <h1>Otros aqui</h1>
       </div>
     </div>
   );
