4afe43a00e910cef2add7a1a6d4b831054c8aa1a
blink <velah027@gmail.com>
2023-10-06 00:05:02 -0300

    Prototipo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from "./logo.svg";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import TextField from '@mui/material/TextField';
 import "./App.css";
 
 function App() {
+  const [alignment, setAlignment] = React.useState('venta');
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          branch dev<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Search">
+          <Box>
+            <ToggleButtonGroup
+              color="primary"
+              value={alignment}
+              exclusive
+              onChange={handleChange}
+              aria-label="Platform"
+              id='opciones'
+            >
+              <ToggleButton value="venta">Venta</ToggleButton>
+              <ToggleButton value="alquiler">Alquiler</ToggleButton>
+              <ToggleButton value="alquilertemporal">Alquiler Temporal</ToggleButton>
+            </ToggleButtonGroup>
+            <TextField id="search" label="Localidad" variant="outlined" />
+          </Box>
+        </div>
       </header>
     </div>
   );
