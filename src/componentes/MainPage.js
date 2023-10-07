7b233e1a6215d9d0168aebe3b04841f89c7cc1f8
blinksore <velah027@gmail.com>
2023-10-06 21:07:53 -0300

    Mover a componentes

---
+++ src/componentes/MainPage.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import TextField from "@mui/material/TextField";
+
+function MainPage() {
+  const [seleccion, setSeleccion] = React.useState("venta");
+
+  const handleChange = (event, newSeleccion) => {
+    setSeleccion(newSeleccion);
+  };
+  return (
+    <div className="App">
+      <div className="Navbar">
+        <h1>Navbar</h1>
+      </div>
+      <div className="SearchBackground">
+        <div className="Search">
+          <Box>
+            <ToggleButtonGroup
+              color="primary"
+              value={seleccion}
+              exclusive
+              onChange={handleChange}
+              aria-label="Platform"
+              id="opciones"
+            >
+              <ToggleButton value="venta">Venta</ToggleButton>
+              <ToggleButton value="alquiler">Alquiler</ToggleButton>
+              <ToggleButton value="alquilertemporal">
+                Alquiler Temporal
+              </ToggleButton>
+            </ToggleButtonGroup>
+            <TextField id="search" label="Localidad" variant="outlined" />
+          </Box>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
