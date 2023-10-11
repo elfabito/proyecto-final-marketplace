1af72c0afe75e0c22f174c5277aa9c3726a68a42
FacundoNunez1 <fakundo1213@gmail.com>
2023-10-11 20:10:43 -0300

    Arreglos

--- src/componentes/feature_propertyDetails/Breadcrumbs.js
+++ src/componentes/feature_propertyDetails/Breadcrumbs.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Link from '@mui/material/Link';
 import { Breadcrumbs } from '@mui/material';
 import "./Styles/Hilo.css"
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export default function ActiveLastBreadcrumb() {
-    const hilos = ["MarcketPlace inmobiliario", "Alquiler", "Maldonado", "Punta Del Este", "Arquiler de monoambiente"];
+export default function ActiveLastBreadcrumb(props) {
+    const [hilo,setHilo]=useState([])
+    const seguimientoHilos = ["MarcketPlace inmobiliario", "Alquiler", "Maldonado", "Punta Del Este", "Arquiler de monoambiente"];
 
+    useEffect(()=>{
+        if (props) {
+        setHilo(props)
+        }else  {
+            setHilo(seguimientoHilos)
+        }
+    },[])
     return (
         <div className='hilo' role="presentation">
             <div>
@@ -15,7 +25,7 @@ export default function ActiveLastBreadcrumb() {
             </div>
             <div>
             <Breadcrumbs aria-label="breadcrumb" separator=">">
-                {hilos.map((item, index) => (
+                {hilo.map((item, index) => (
                     <Link key={index} style={linkStyle} underline="hover" color="inherit" href="/">
                     <span>{item}</span>
                     </Link>
