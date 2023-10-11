adcbe76082e8782a97d5288034c6b566a8f3a78a
Fabs <elfabito@gmail.com>
2023-10-11 13:11:03 -0300

    Form Message button

---
+++ src/componentes/FormMessage.js
@@ -0,0 +1,78 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+export default function FormMessage() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Consultar
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Consultar al anunciante
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent>
+          <TextField id="outlined-basic" label="Nombre" variant="outlined" />
+          first
+          <TextField id="outlined-basic" label="Apellido" variant="outlined" />
+          last
+          <TextField fullWidth label="fullWidth" id="fullWidth" />
+          email
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Multiline"
+            multiline
+            maxRows={4}
+          />
+          {/* <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          /> */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Enviar Consulta</Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </div>
+  );
+}
