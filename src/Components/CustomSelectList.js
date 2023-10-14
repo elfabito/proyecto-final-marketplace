58460cd2319a01e83c8687e1362ab8e5b8dc20ba
Daniel Orlinski <67550333+danielorlinski@users.noreply.github.com>
2023-10-13 21:31:20 -0300

    Error solving and fixes to the main page

--- src/Components/CustomSelectList.js
+++ src/Components/CustomSelectList.js
@@ -1,16 +1,15 @@
-import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import Chip from "@mui/material/Chip";
-import { useEffect } from "react";
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Chip from '@mui/material/Chip'
+import { useEffect } from 'react'
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
 const MenuProps = {
   PaperProps: {
     style: {
@@ -18,62 +17,53 @@ const MenuProps = {
       width: 250,
     },
   },
-};
+}
 
 const options_default = [
-  "Casa",
-  "Apartamento",
-  "Terreno",
-  "Local Comercial",
-  "Oficina",
-  "Chacra o Campo",
-  "Garage o Cochera",
-];
+  'Casa',
+  'Apartamento',
+  'Terreno',
+  'Local Comercial',
+  'Oficina',
+  'Chacra o Campo',
+  'Garage o Cochera',
+]
 
-// function getStyles(option, optionName, theme) {
-//   return {
-//     fontWeight:
-//       optionName.indexOf(option) === -1
-//         ? theme.typography.fontWeightRegular
-//         : theme.typography.fontWeightMedium,
-//   };
-// }
 function CustomSelectList(props) {
-  // const theme = useTheme();
-  const [optionName, setOptionName] = React.useState([]);
-  const [options, setOptions] = React.useState([]);
+  const [optionName, setOptionName] = React.useState([])
+  const [options, setOptions] = React.useState([])
   const handleChange = (event) => {
     const {
       target: { value },
-    } = event;
+    } = event
     setOptionName(
       // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
+      typeof value === 'string' ? value.split(',') : value
+    )
+  }
   useEffect(() => {
     if (props.options) {
-      setOptions(props.options);
+      setOptions(props.options)
     } else {
-      setOptions(options_default);
+      setOptions(options_default)
     }
-  }, []);
+  }, [])
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="Lista">Selección</InputLabel>
+        <InputLabel id='Lista'>Selección</InputLabel>
         <Select
-          labelId="Lista"
-          id="Lista"
+          labelId='Lista'
+          id='Lista'
           multiple
           value={optionName}
           onChange={handleChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Selección" />}
+          input={<OutlinedInput id='select-multiple-chip' label='Selección' />}
           renderValue={(selected) => (
             console.log(selected),
             console.log(selected.length),
             (
-              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
                 {selected.map((value) => (
                   <Chip key={value} label={value} />
                 ))}
@@ -90,8 +80,8 @@ function CustomSelectList(props) {
         </Select>
       </FormControl>
     </div>
-  );
+  )
 }
 
 // eslint-disable-next-line no-undef
-export default CustomSelectList;
+export default CustomSelectList
