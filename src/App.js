55061cd7bdfb1f823f268efac8e7162e5e41504f
blinkodemia <velah027@gmail.com>
2023-10-23 19:39:04 -0300

    Pull from dev

--- src/App.js
+++ src/App.js
@@ -7,21 +7,12 @@ import { BrowserRouter as Router, Outlet } from 'react-router-dom'
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Outlet />
-      </div> 
-    </div>
-=======
       <div className='App'>
         <Navbar />
         <div className='content'>
           <Outlet />
         </div>
       </div>
->>>>>>> a4ae650ed21a8e0b3fa23e4c8a34767f24519d81
     </Router>
   )
 }
