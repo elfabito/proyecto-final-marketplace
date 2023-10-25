6fb0cd258e0c797f53d2585dff0801a3fe5b2328
blinkodemia <velah027@gmail.com>
2023-10-25 18:03:22 -0300

    Oops, typo error

---
+++ src/Components/Footer.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function Footer() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Item>xs</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+        <Grid item xs>
+          <Item>xs</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -55,42 +55,7 @@ function MainPage() {
       </div>
       <div className="carrousel-container">
         <Carrousel />
-      </div>
-      <footer>
-        <div id="footer">
-          <div className="about">
-            <h4>
-              <a href="#">Sobre Nosotros</a>
-            </h4>
-            <h4>
-              <a href="#">Terminos y Condiciones</a>
-            </h4>
-          </div>
-          <div className="tipos">
-            <h4>
-              <a href="#">Alquiler</a>
-            </h4>
-            <h4>
-              <a href="#">Alquiler Temporal</a>
-            </h4>
-            <h4>
-              <a href="#">Ventas</a>
-            </h4>
-          </div>
-          <div className="socials">
-            <h4>
-              <FaYoutube /> <a href="#">Youtube</a>
-            </h4>
-            <h4>
-              <FaFacebook /> <a href="#">Facebook</a>
-            </h4>
-            <h4>
-              <FaTwitter /> <a href="#">Twitter</a>
-            </h4>
-          </div>
-        </div>
-      </footer>
-    </div>
+      <div/>
   );
 }
 
